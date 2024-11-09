be6260b00eb8b80349535f286bbfddc13531c8c3
vilmelhina <vilhelmina2.0@gmail.com>
2024-11-09 21:52:29 +0100

    Separate file for LEDs

---
+++ leds.py
@@ -0,0 +1,35 @@
+from picozero import pico_led, LED, Button
+from time import sleep
+
+green_led = LED(1)
+red_led = LED(14)
+button = Button(6)
+
+def start_green():
+    green_led.on()
+
+def stop_green():
+    green_led.off()
+
+def start_red():
+    red_led.on()
+
+def stop_red():
+    red_led.off()
+
+def blink():
+    start_all()
+    sleep(1)
+    stop_all()
+
+def start_all():
+    print('start')
+    pico_led.on()
+    start_green()
+    start_red()
+
+def stop_all():
+    print('stop')
+    pico_led.off()
+    stop_green()
+    stop_red()
--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-from picozero import pinout, pico_led, LED
-from time import sleep
+from leds import blink
+from picozero import pinout
 
-def flash(led):
-    led.on()
-    sleep(1)
-    led.off()
-
-green_led = LED(1)
-red_led = LED(14)
-
-flash(pico_led)
-flash(green_led)
-flash(red_led)
+print('Hello Pico!')
+pinout()
+blink()
