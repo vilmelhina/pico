8bec9e3e805a3260212bdd5bffd96316c1e0b1e5
vilmelhina <vilhelmina2.0@gmail.com>
2024-11-16 13:03:11 +0100

    Use microdot for server

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from leds import blink
-import machine
-from server import connect_to_wifi, open_socket, serve
-
+from server import connect_to_wifi, serve
 
 print("Hello Pico!")
 blink()
 
 try:
-    ip = connect_to_wifi()
-    connection = open_socket(ip)
-    serve(connection)
+    connect_to_wifi()
+    serve()
 except KeyboardInterrupt:
     pass
--- server.py
+++ server.py
@@ -1,14 +1,13 @@
 from time import sleep
 import network
-import socket
-import os
 from my_config import ssid, password
 from leds import start_green, stop_green, start_red, stop_red
+from microdot import Microdot
 
 
 def connect_to_wifi():
-    print("Connecting to WIFI...")
     wlan = network.WLAN(network.STA_IF)
+    print("Connecting to WIFI...")
     wlan.active(True)
     wlan.connect(ssid, password)
     while not wlan.isconnected():
@@ -16,40 +15,33 @@ def connect_to_wifi():
         sleep(1)
     ip = wlan.ifconfig()[0]
     print(f"Connected on {ip}")
-    return ip
 
 
-def open_socket(ip):
-    address = (ip, 80)
-    connection = socket.socket()
-    connection.bind(address)
-    connection.listen(1)
-    return connection
+def serve():
+    app = Microdot()
 
+    @app.route("/")
+    async def index(request):
+        return "Hello, world!"
 
-def serve(connection):
-    print("Serving...")
-    while True:
-        client = connection.accept()[0]
-        request = client.recv(1024)
-        request = str(request)
-        print(request)
-        try:
-            print(parseRequest(request))
-            request = request.split(" ")[1]
-        except IndexError:
-            print("error")
-            pass
-        if request == "/redLED":
-            print("RED LED")
-        elif request == "/greenLED":
-            print("GREEN LED")
-        client.close()
+    @app.post("/redLED")
+    async def postRedLed(request):
+        if request.json.get("state") == "ON":
+            start_red()
+            return {"success": True}
+        if request.json.get("state") == "OFF":
+            stop_red()
+            return {"success": True}
+        return "Bad request", 400
 
+    @app.post("/greenLED")
+    async def postGreenLed(request):
+        if request.json.get("state") == "ON":
+            start_green()
+            return {"success": True}
+        if request.json.get("state") == "OFF":
+            stop_green()
+            return {"success": True}
+        return "Bad request", 400
 
-def parseRequest(request):
-    lines = request.split("\r\n")
-    method = lines[0].split(" ")[0]
-    path = lines[0].split(" ")[1]
-    body = lines[-1]
-    return {"method": method, "path": path, "body": body}
+    app.run()
